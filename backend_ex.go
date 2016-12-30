e404e646c2ec7bc9807b0a68ad703e981a5ec6ea
bitter41 <bitter41@gmail.com>
2016-12-31 01:30:12 +0300

    simple ping-pong

--- backend_ex.go
+++ backend_ex.go
@@ -3,11 +3,38 @@ package main
 //import "fmt"
 import "log"
 import "time"
-import "github.com/go-telegram-bot-api/telegram-bot-api"
+import (
+	"github.com/go-telegram-bot-api/telegram-bot-api"
+	"strconv"
+)
+
 
 const bot_token  = "317035683:AAFvveEwHzO1wy-Rdqj6jCBjBI4msED2CLc"
 
+type Activity struct {
+	StartDateTime 	time.Time
+	Type 		string
+	Usermane	string
+}
+
+type User struct {
+	UserID 		int
+	Username 	string
+}
+
+func (u User) isDbSynced() int {
+
+	return 1
+}
+
+func (u User) updateInDB() int {
+
+	return 1
+}
+
 func main() {
+	Activities := make( map[int]*Activity )
+
 	bot, err := tgbotapi.NewBotAPI( bot_token )
 	if err != nil {
 		log.Panic(err)
@@ -30,9 +57,23 @@ func main() {
 
 		log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
 
-		msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
-		msg.ReplyToMessageID = update.Message.MessageID
+		if _, ok := Activities[ update.Message.From.ID ]; ok {
+			msg := tgbotapi.NewMessage(update.Message.Chat.ID, "work already started by " + strconv.Itoa( update.Message.From.ID))
+			bot.Send(msg)
+		} else {
+			Activities[ update.Message.From.ID ] = &Activity{time.Now(), "work", update.Message.From.UserName }
+			msg := tgbotapi.NewMessage(update.Message.Chat.ID, "work start for " + strconv.Itoa( update.Message.From.ID))
+			bot.Send(msg)
+		}
 
-		bot.Send(msg)
+		//msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
+		//msg.ReplyToMessageID = update.Message.MessageID
+
+
 	}
 }
+
+
+
+
+
