a7817617f5feaa0e1d67057e29127a450f4d4f4e
Romancha <Romanchabest55@gmail.com>
2017-01-17 16:23:41 +0700

    types, inline buttons

--- types/event.go
+++
@@ -1,9 +0,0 @@
-package types
-
-import "time"
-
-type Event struct {
-	id int
-	time time.Time
-	eventType string
-}
--- types/user.go
+++
@@ -1,6 +0,0 @@
-package types
-
-type User struct {
-	Id int
-	UserName string
-}
